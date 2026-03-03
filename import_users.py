d8355d9d92b0028aa34417933f8e17fef3236a8a
valen <meowww717@gmail.com>
2026-03-03 19:44:46 +0200

    reverted

--- import_users.py
+++ import_users.py
@@ -1,5 +1,6 @@
 from telethon import TelegramClient
 import asyncio
+import os
 import sys
 
 # Фікс для Windows консолі
@@ -8,8 +9,7 @@ sys.stdout.reconfigure(encoding='utf-8')
 api_id = int(os.getenv("TELEGRAM_API_ID"))
 api_hash = os.getenv("TELEGRAM_API_HASH")
 
-api_id = 33835492
-api_hash = "8085a2ec391ff98c8185cf0f741945eb"
+
 group = "https://t.me/+sHiPUqG2ApY5MTgy"
 
 client = TelegramClient("session", api_id, api_hash)
@@ -18,11 +18,7 @@ client = TelegramClient("session", api_id, api_hash)
 async def get_users():
     await client.start()
     participants = await client.get_participants(group)
-    print("Учасники групи:\n")
     for u in participants:
-        if u.bot:
-            continue
-        print(
-            f"ID: {u.id} | {u.first_name} {u.last_name or ''} | @{u.username or '-'}")
+        print(u.id, u.first_name, u.username)
 
 asyncio.run(get_users())
